aebf19691cf48a8ac4f0e4fb8bf4313fa13faf88
SaeedDev94 <saeedp47@gmail.com>
2024-12-13 01:36:32 -0700

    Client#media

--- client.ts
+++ client.ts
@@ -1,11 +1,69 @@
 #!/usr/bin/env node
 
-import { Api, TelegramClient } from 'telegram';
+import { createInterface } from 'readline';
+import { writeFileSync } from 'fs';
+import { TelegramClient } from 'telegram';
 import { StoreSession } from 'telegram/sessions';
 import { TelegramClientParams } from 'telegram/client/telegramBaseClient';
-import { createInterface } from 'readline';
 import { apiHash, apiId, phoneNumber } from './env';
-import User = Api.User;
+
+class Client {
+
+  async main(args: string[]): Promise<void> {
+    console.log('Starting Telegram client...');
+    await client.start({
+      phoneNumber: async () => phoneNumber,
+      phoneCode: async () => await input('Enter OTP:'),
+      password: async () => await input('Enter 2AF:'),
+      onError: (err: Error) => console.error(err),
+    });
+    client.session.save();
+    console.log('Connected to Telegram!');
+    const me = await client.getMe();
+    console.log({
+      id: me.id,
+      username: me.username,
+    });
+    switch (args[0]) {
+      case 'media':
+        await this.media(new URL(args[1]));
+        break;
+    }
+  }
+
+  private async media(link: URL) {
+    const parts = link.pathname.split('/').slice(1);
+    const entity = await client.getEntity(parts[0]);
+    const messages = await client.getMessages(entity, {
+      ids: Number(parts[1]),
+    });
+    if (!messages.length) throw new Error(`No messages found for ${link.toString()}`);
+    for (const message of messages) {
+      if (!message.media) {
+        console.warn(`Message #${message.id} has no media!`);
+        return;
+      }
+      console.log(`Downloading #${message.id} media:`);
+      const path = mediaPath(message.id);
+      const file = await client.downloadMedia(message.media, {
+        progressCallback: (downloaded, total) => {
+          const progress = downloaded.divide(total).multiply(100);
+          process.stdout.clearLine(0);
+          process.stdout.cursorTo(0);
+          process.stdout.write(`${progress}%`);
+        },
+      });
+      process.stdout.write('\n');
+      if (!file) {
+        console.warn(`Failed to download #${message.id} media!`);
+        return;
+      }
+      writeFileSync(path, file);
+      console.log(`#${message.id} downloaded successfully: ${path}`);
+    }
+  }
+
+}
 
 const input = (question: string, options?: string[]): Promise<string> => {
   return new Promise((resolve) => {
@@ -17,34 +75,25 @@ const input = (question: string, options?: string[]): Promise<string> => {
       resolve(answer.trim());
     });
   });
-}
+};
 
-class Client {
-  async main(): Promise<void> {
-    const session: StoreSession = new StoreSession('auth');
-    const params: TelegramClientParams = {
-      connectionRetries: 5,
-    };
-    console.log('Starting Telegram client...');
-    const client: TelegramClient = new TelegramClient(session, apiId, apiHash, params);
-    await client.start({
-      phoneNumber: async () => phoneNumber,
-      phoneCode: async () => await input('Enter OTP:'),
-      password: async () => await input('Enter 2AF:'),
-      onError: (err: Error) => console.error(err),
-    });
-    client.session.save();
-    console.log('Connected to Telegram!');
-    const me: User = await client.getMe();
-    console.log({
-      id: me.id,
-      username: me.username,
-    });
-    await client.disconnect();
-    console.log('Client disconnected.');
-  }
-}
+const session: StoreSession = new StoreSession('auth');
+const params: TelegramClientParams = {
+  connectionRetries: 5,
+};
+const client: TelegramClient = new TelegramClient(session, apiId, apiHash, params);
+const disconnect = async () => {
+  await client.disconnect();
+  console.log('Client disconnected.');
+};
+const mediaPath = (name: any) => `${__dirname}/media/${name}`;
 
 new Client()
-  .main()
-  .catch((err: Error) => console.error(err));
+  .main(process.argv.slice(2))
+  .then(async () => {
+    await disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await disconnect();
+  });
